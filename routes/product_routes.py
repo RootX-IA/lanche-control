8058ed6bc94cf70c8fc9828a14094c8b338a7b30
Alan <alan-alvessilva@outlook.com>
2026-04-26 15:00:34 -0300

    feat: add product update endpoint

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -1,7 +1,15 @@
 #ENTRADA HTTP (FLASK)
 
 from flask import Blueprint
-from controllers.product_controller import criar_produto
+from controllers.product_controller import (
+    criar_produto,
+    buscar_produtos,
+    buscar_produto,
+    atualizar_produto,
+)
 produto_bp = Blueprint("produto_bp", __name__)
 
 produto_bp.route("/produtos", methods=["POST"])(criar_produto)
+produto_bp.route("/produtos", methods=["GET"])(buscar_produtos)
+produto_bp.route("/produtos/<int:id>", methods=["GET"])(buscar_produto)
+produto_bp.route("/produtos/<int:id>", methods=["PATCH"])(atualizar_produto)
